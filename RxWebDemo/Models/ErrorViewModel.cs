06e2a5ac1cdd11d1662aa8ab8224ce587c36eba1
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-04 01:57:12 +0330

    Add project files.

---
+++ RxWebDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RxWebDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
